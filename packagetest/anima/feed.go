467eee25cc793ab86d337b3f0fb48d669033c1f9
tokumoko <76512416+tokumoko@users.noreply.github.com>
2022-05-20 21:54:17 +0900

    20220520

---
+++ packagetest/anima/feed.go
@@ -0,0 +1,11 @@
+package anima
+
+func Abc() string {
+	return "KUBA"
+}
+func Monkey() string {
+	return "KA"
+}
+func Kuruit() string {
+	return "www"
+}
---
+++ packagetest/test.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"./anima"
+)
+
+func main() {
+	fmt.Println(anima.Abc())
+
+	fmt.Println(anima.Monkey())
+
+	fmt.Println(anima.Kuruit())
+}
